09a86ff245dbcf6e87b818986fd5a7df1acc2a90
ejin1018 <amy1018@naver.com>
2023-04-26 17:29:22 +0900

    230426 form axios post 까지 작업

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,23 @@
 <template>
-  <div class="choose-wrap">
-    <form action="post" class="choose-container">
-      <div class="choose-container-left">
-        <label class="choose-when">
-          <input type="date" v-model="meetDate" v-on:click="getDate">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="none"><path d="M24 0v24H0V0h24ZM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018Zm.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022Zm-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01l-.184-.092Z"/><path fill="currentColor" d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-7h18Zm-5-9a1 1 0 0 1 1 1v1h2a2 2 0 0 1 2 2v3H3V7a2 2 0 0 1 2-2h2V4a1 1 0 0 1 2 0v1h6V4a1 1 0 0 1 1-1Z"/></g></svg>
-          <p>{{ meetDate }}</p>
-        </label>
-      </div>
-      <div class="choose-container-middle">
-        <label class="choose-location">
-          <input type="text" placeholder="지역">
-        </label>
-        <label class="choose-cafe">
-          <input type="text" placeholder="카페">
-        </label>
-      </div>
-      <div class="choose-container-right">
-        <select name="cafe_turn_name" id="cafe_turn">
-          <option value="현우">현우</option>
-          <option value="규연">규연</option>
-          <option value="애경">애경</option>
-          <option value="이진">이진</option>
-        </select>
-        <button type="submit">등록</button>
-      </div>
-    </form>
-  </div>
+  <meetForm />
+  <meetRecord />
 </template>
 
 <script>
-import {ref} from "vue";
+import axios from "axios";
+import meetForm from "@/components/meetForm.vue";
+import meetRecord from "@/components/meetRecord.vue";
 export default {
+  components:{meetForm,meetRecord},
   setup(){
-    let meetDate = ref('');
-    let getDate = ()=>{
-      console.log(meetDate.value);
-    }
-    return {meetDate,getDate}
+    const url = "https://port-0-react-mangoshop-server-6g2llfg440fy.sel3.cloudtype.app";
+    const paramMeets = "/meets";
+
+    axios.get(`${url}${paramMeets}`).then((success)=>{
+      console.log(success.data)
+    }).catch((error)=>{
+      console.log(error);
+    })
   }
 }
 </script>
